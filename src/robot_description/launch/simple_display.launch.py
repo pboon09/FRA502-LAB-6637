77d5ff0c8548ee8392c2955c73a9ef54a1a5bca0
pboon09 <pboonchanachai@gmail.com>
2025-11-12 18:03:33 +0700

    Update simple_display.launch.py

--- src/robot_description/launch/simple_display.launch.py
+++ src/robot_description/launch/simple_display.launch.py
@@ -53,6 +53,6 @@ def generate_launch_description():
     
     launch_description.add_action(rviz)
     launch_description.add_action(robot_state_publisher)
-    launch_description.add_action(joint_state_publisher_gui)
+    # launch_description.add_action(joint_state_publisher_gui)
     
     return launch_description
